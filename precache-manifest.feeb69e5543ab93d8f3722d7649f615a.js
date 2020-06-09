e015ab8e023884cea94af8d5776347e823feeb80
kevin <changheeloov@gmail.com>
2020-06-09 17:08:38 -0300

    Updates

---
+++ precache-manifest.feeb69e5543ab93d8f3722d7649f615a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4e81eb5eb8e28da9cf23138646e56e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "35073f19ac668ca79779",
+    "url": "/movie_app_2020/static/css/main.689595c0.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35073f19ac668ca79779",
+    "url": "/movie_app_2020/static/js/main.49b143c1.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
